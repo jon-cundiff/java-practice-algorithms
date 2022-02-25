f9d52cbfbd33c6e8cee672abe06a60e7f14daa0d
jon-cundiff <jon.cundiff@gmail.com>
2022-02-25 09:17:33 -0600

    Implements Bubble Sort properly, was not comparing adjacent elements

--- src/com/jc/Sorting.java
+++ src/com/jc/Sorting.java
@@ -2,13 +2,23 @@ package com.jc;
 
 public class Sorting {
     public static void bubbleSort(int[] arr, boolean isAscending) {
+        // Time Complexity = N loops * N loops = O(N^2)
+        // Space Complexity = O(N)
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if ((arr[i] < arr[j] && isAscending) || (arr[i] > arr[j] && !isAscending)) {
-                    int temp = arr[i] ;
-                    arr[i]  = arr[j];
+            // If no swaps in inner loop, then already sorted
+            boolean swapped = false;
+
+            // Each iteration of i moves largest to the end
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if ((arr[j] > arr[j + 1] && isAscending) || (arr[j] < arr[j + 1] && !isAscending)) {
+                    int temp = arr[j + 1] ;
+                    arr[j + 1]  = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                return;
             }
         }
     }

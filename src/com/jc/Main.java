4a7461df1513cd8011266e9d49cc795e1fd72934
jon-cundiff <jon.cundiff@gmail.com>
2022-02-25 09:41:23 -0600

    Refactors to reset initial array to reuse on other sorting algorithms

--- src/com/jc/Main.java
+++ src/com/jc/Main.java
@@ -3,16 +3,31 @@ package com.jc;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void printArr(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] cloneSample(int sampNum) {
         int[] sampleArray = {5, 23, 12, 1, 43, -5, 3 ,0, 54};
-        int[] sampleArray2 = {-8, 11, 5, 23, 12, 1, 43, -5, 3 ,0, 54, 43, 3, -1};
+        int[] bubble2 = {-8, 11, 5, 23, 12, 1, 43, -5, 3 ,0, 54, 43, 3, -1};
 
-        System.out.println(Arrays.toString(sampleArray));
-        Sorting.bubbleSort(sampleArray, true);
-        System.out.println(Arrays.toString(sampleArray));
+        return sampNum == 1 ? sampleArray : bubble2;
+    }
 
-        System.out.println(Arrays.toString(sampleArray2));
-        Sorting.bubbleSort(sampleArray2, false);
-        System.out.println(Arrays.toString(sampleArray2));
+    public static void main(String[] args) {
+        Sorting sort = new Sorting();
+
+        System.out.println("Bubble Sort");
+        var bubble1 = cloneSample(1);
+        System.out.println(Arrays.toString(bubble1));
+        sort.bubbleSort(bubble1, true);
+        System.out.println(Arrays.toString(bubble1));
+
+        var bubble2 = cloneSample(2);
+        System.out.println(Arrays.toString(bubble2));
+        sort.bubbleSort(bubble2, false);
+        System.out.println(Arrays.toString(bubble2));
+
+        System.out.println("\nSelection Sort");
     }
 }
--- src/com/jc/Sorting.java
+++ src/com/jc/Sorting.java
@@ -1,7 +1,11 @@
 package com.jc;
 
 public class Sorting {
-    public static void bubbleSort(int[] arr, boolean isAscending) {
+    private boolean shouldSwap(int val1, int val2, boolean isAscending) {
+        return isAscending ? val1 > val2 : val1 < val2;
+    }
+
+    public void bubbleSort(int[] arr, boolean isAscending) {
         // Time Complexity = N loops * N loops = O(N^2)
         // Space Complexity = O(N)
         for (int i = 0; i < arr.length; i++) {
@@ -10,7 +14,7 @@ public class Sorting {
 
             // Each iteration of i moves largest to the end
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if ((arr[j] > arr[j + 1] && isAscending) || (arr[j] < arr[j + 1] && !isAscending)) {
+                if (shouldSwap(arr[j], arr[j + 1], isAscending)) {
                     int temp = arr[j + 1] ;
                     arr[j + 1]  = arr[j];
                     arr[j] = temp;
